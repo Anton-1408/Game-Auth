571ea2b1db5a83ce75766fd1c4b5d92a28bd201b
Anton-1408 <antonvotinov@gmail.com>
2023-02-13 20:10:32 +0600

    Initial Commit

---
+++ PockerController/PockerControllerApp.swift
@@ -0,0 +1,17 @@
+//
+//  PockerControllerApp.swift
+//  PockerController
+//
+//  Created by Anton Votinov on 13.02.2023.
+//
+
+import SwiftUI
+
+@main
+struct PockerControllerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
